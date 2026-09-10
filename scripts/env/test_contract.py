295e0bd12adabcad2c49204843cee3bbf3a24be6
Miguel Salinas <mikeys@me.com>
2026-09-10 16:02:29 -0500

    Advance the uikit vendor pin and env contract after merging agent/nsobject-value-classes2
    EXPECTED_INREPO_UIKIT_TREE cb1b490bd918b10b509556873ee93c1f908ddb89 -> 21ad0574ce004a259a46df02c52f4aa5cd41be3a.

--- scripts/env/test_contract.py
+++ scripts/env/test_contract.py
@@ -72,7 +72,7 @@ class ContractLockTests(unittest.TestCase):
         checkouts = checkouts_by_id(load_contract(ROOT))
         self.assertEqual(
             checkouts["uikit-inrepo"]["tree"],
-            "cb1b490bd918b10b509556873ee93c1f908ddb89",
+            "21ad0574ce004a259a46df02c52f4aa5cd41be3a",
         )
         self.assertEqual(
             checkouts["machorun-inrepo"]["tree"],
